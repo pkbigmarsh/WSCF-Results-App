6682fd2437f540d82d27a8f8fd9d9d863224c2b2
Marshall Sumwalt <marshallsumwalt@gmail.com>
2014-06-18 21:15:52 -0500

    Can write actually information to a PDF, but there isn't any formatting yet

--- Project/CSVReader.py
+++ Project/CSVReader.py
@@ -15,14 +15,24 @@ class CSVReader():
 
     def getNext(self):
         newLine = self.csv_file.readline()
-        if (newLine is None):
+        if (newLine is None or not newLine):
             return None
-        newRow = safeSplit(self.csv_file.readline(), ",")
+        newRow = safeSplit(newLine, ",")
         newMap = {}
         for i in range(0, len(newRow)):
             newMap[self.headers[i]] = newRow[i].strip().rstrip().strip("\"").rstrip("\"")
 
         return newMap
+
+    def getNextAsString(self):
+        row = self.getNext()
+        if row is None:
+            return None
+
+        return_str = ""
+        for key in self.headers:
+            return_str += row[key] + " "
+        return return_str
 
 def safeSplit(string, delimiter):
     start = 0
--- Project/main.py
+++ Project/main.py
@@ -1,8 +1,62 @@
 import os
 from CSVReader import *
+from reportlab.pdfgen import canvas
+
+TEXT = """%s    page %d of %d
+
+a wonderful file
+created with Sample_Code/makesimple.py"""
+
+POINT = 1
+INCH = 72
+WIDTH = 11 * INCH
+HEIGHT = 8.5 * INCH
+
+
 
 individualFilePath = '../Info/tournamentresultsposting/individual_k12.TXT'
+outputFileName = "test.pdf"
 
 result = CSVReader(individualFilePath)
-print result.headers
-print result.getNext()
+
+x = 1 * INCH
+y = 8 * INCH
+
+c = canvas.Canvas(outputFileName, pagesize=(WIDTH, HEIGHT))
+c.setStrokeColorRGB(0,100,0)
+c.setFillColorRGB(0,100,0)
+c.rect(x,y,5 * INCH, 12 * POINT, fill=1)
+
+c.setStrokeColorRGB(0,0,0)
+c.setFillColorRGB(0,0,0)
+
+c.setFont("Helvetica", 12 * POINT)
+
+next = result.getNextAsString()
+while(next != None):
+    if (y < (.5 * INCH)):
+        y = 8 * INCH
+        c.showPage()
+
+    c.drawString(x, y, next)
+    y -= 12 * POINT
+
+    next = result.getNextAsString()
+
+c.save()
+
+def make_pdf_file(output_filename, np):
+    title = output_filename
+    c = canvas.Canvas(output_filename, pagesize=(8.5 * inch, 11 * inch))
+    c.setStrokeColorRGB(0,0,0)
+    c.setFillColorRGB(0,0,0)
+    c.setFont("Helvetica", 12 * point) 
+    for pn in range(1, np + 1):
+        v = 10 * inch
+        for subtline in (TEXT % (output_filename, pn, np)).split( '\n' ):
+            c.drawString( 1 * inch, v, subtline )
+            v -= 12 * point
+        c.showPage()
+    c.save()
+
+# make_pdf_file(outputFileName, 1)
